f5152a2f1b37bd039881a00e06068f6709693a06
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-01-05 02:55:55 +0200

    Compelted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsListFilter from './ContactsListFilter/ContactsListFilter';
+import ContactsList from './ContactsList/ContactsList';
+
+const INITIAL_STATE = {
+  filter: '',
+  contacts: [],
 };
+
+export default class App extends Component {
+  state = { ...INITIAL_STATE };
+
+  onChange = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
+
+  onDelete = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== contactId),
+    });
+  };
+
+  addContact = newContact => {
+    if (this.isContactAlreadyAdded(newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  isContactAlreadyAdded = name => {
+    if (
+      this.state.contacts.filter(
+        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      ).length > 0
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  searchResult = keyword => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+
+        {this.state.contacts.length > 0 && (
+          <Section title="Contacts">
+            <ContactsListFilter onChange={this.onChange} />
+            <ContactsList
+              contacts={this.searchResult(this.state.filter)}
+              onDelete={this.onDelete}
+            />
+          </Section>
+        )}
+      </>
+    );
+  }
+}
